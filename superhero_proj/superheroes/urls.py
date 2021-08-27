98c0584fb608866a492614de431f7263589ef28e
Vernon Roberts <nakkiasr@live.com>
2021-08-26 23:42:19 -0700

    Trying to add edit file and get delete button to work.

--- superhero_proj/superheroes/urls.py
+++ superhero_proj/superheroes/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib import admin
 
 from .import views
 
@@ -6,5 +7,6 @@ app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:hero_id>/', views.detail, name='detail'),
-    path('new/', views.create, name='create')
+    path('new/', views.create, name='create'),
+    path('edit/', views.edit, name='edit')
 ]
--- superhero_proj/superheroes/views.py
+++ superhero_proj/superheroes/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import Superhero
+
 
 # Create your views here.
 def index(request):
@@ -30,3 +31,17 @@ def create(request):
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         return render(request, 'superheroes/create.html')
+
+def edit(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+        current_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+        current_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        return render(request, 'superheroes/create.html')
+    
